6c123d173aca9fcecaa4453e3fe5f3bc618a2837
Velo <hartley.max@outlook.com>
2021-04-04 21:41:43 +0100

    home page has list of all blogs

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,61 +1,38 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/index.module.css';
 
-export default function Home() {
+export async function getStaticProps(context) {
+	const blogs = await fetch('http://localhost:4000/api/blogs', {
+		method: 'GET',
+	}).then((res) => res.json());
+	return {
+		props: {
+			pages: blogs,
+		}, // will be passed to the page component as props
+	};
+}
+
+export default function Home(props) {
+	// console.log(props);
 	return (
-		<div className={styles.container}>
+		<>
 			<Head>
 				<title>Create Next App</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-
 			<main className={styles.main}>
-				<h1 className={styles.title}>
-					Welcome to <a href='https://nextjs.org'>Next.js!</a>
-				</h1>
-
-				<p className={styles.description}>
-					Get started by editing <code className={styles.code}>pages/index.js</code>
-				</p>
-
-				<div className={styles.grid}>
-					<a href='https://nextjs.org/docs' className={styles.card}>
-						<h3>Documentation &rarr;</h3>
-						<p>Find in-depth information about Next.js features and API.</p>
-					</a>
-
-					<a href='https://nextjs.org/learn' className={styles.card}>
-						<h3>Learn &rarr;</h3>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a
-						href='https://github.com/vercel/next.js/tree/master/examples'
-						className={styles.card}
-					>
-						<h3>Examples &rarr;</h3>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-						className={styles.card}
-					>
-						<h3>Deploy &rarr;</h3>
-						<p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-					</a>
-				</div>
+				<h1>Moshi Moshi. Blog Desu.</h1>
+				<ul>
+					{props.pages.blogs.map((page) => (
+						<li key={page.title.split(' ').join('_')}>
+							<Link href={`/blogs/${page.title.split(' ').join('_')}`}>
+								<a>{page.title}</a>
+							</Link>
+						</li>
+					))}
+				</ul>
 			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Powered by <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-				</a>
-			</footer>
-		</div>
+		</>
 	);
 }
